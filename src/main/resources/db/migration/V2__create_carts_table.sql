8a9b0b8ae7f2ffb2a15c676857e0e9d3e78215f9
yamchips <wflunch@gmail.com>
2025-10-05 22:22:45 +0100

    Create carts items table

--- src/main/resources/db/migration/V2__create_carts_table.sql
+++ src/main/resources/db/migration/V2__create_carts_table.sql
@@ -3,3 +3,13 @@ CREATE TABLE store_api.carts (
     date_created DATE DEFAULT (CURDATE()) NOT NULL,
     CONSTRAINT Cart_PK PRIMARY KEY (id)
 );
+CREATE TABLE store_api.cart_items (
+	id BIGINT auto_increment NOT NULL,
+	cart_id BINARY(16) NOT NULL,
+	product_id BIGINT NOT NULL,
+	quantity INT DEFAULT 1 NOT NULL,
+	CONSTRAINT cart_items_pk PRIMARY KEY (id),
+	CONSTRAINT cart_items_cart_product_unique UNIQUE KEY (cart_id,product_id),
+	CONSTRAINT cart_items_carts_FK FOREIGN KEY (cart_id) REFERENCES store_api.carts(id) ON DELETE CASCADE,
+	CONSTRAINT cart_items_products_FK FOREIGN KEY (product_id) REFERENCES store_api.products(id) ON DELETE CASCADE
+);
