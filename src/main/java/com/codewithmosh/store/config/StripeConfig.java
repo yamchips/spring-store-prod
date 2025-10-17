2fc7b21f5f0788ad572f2f9b0dd72e48344c7704
yamchips <wflunch@gmail.com>
2025-10-17 17:06:50 +0100

    Add strip to project

---
+++ src/main/java/com/codewithmosh/store/config/StripeConfig.java
@@ -0,0 +1,19 @@
+package com.codewithmosh.store.config;
+
+import com.stripe.Stripe;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StripeConfig {
+
+    @Value("${stripe.secretKey}")
+    private String secretKey;
+
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = secretKey;
+    }
+
+}
