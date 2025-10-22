433091e23015a429f3186fa787b2d5d3c209e6c1
yamchips <wflunch@gmail.com>
2025-10-22 20:52:19 +0100

    Build and test webhook endpoint

--- src/main/java/com/codewithmosh/store/config/SecurityConfig.java
+++ src/main/java/com/codewithmosh/store/config/SecurityConfig.java
@@ -63,6 +63,7 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.POST, "/users").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/auth/refresh").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/checkout/webhook").permitAll()
                                 .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(c ->
--- src/main/java/com/codewithmosh/store/controllers/CheckOutController.java
+++ src/main/java/com/codewithmosh/store/controllers/CheckOutController.java
@@ -7,25 +7,56 @@ import com.codewithmosh.store.exceptions.CartEmptyException;
 import com.codewithmosh.store.exceptions.CartNotFoundException;
 import com.codewithmosh.store.exceptions.PaymentException;
 import com.codewithmosh.store.services.CheckoutService;
+import com.stripe.exception.SignatureVerificationException;
 import com.stripe.exception.StripeException;
+import com.stripe.model.Event;
+import com.stripe.model.StripeObject;
+import com.stripe.net.Webhook;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/checkout")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CheckOutController {
 
     private final CheckoutService checkoutService;
+
+    @Value("${stripe.webhookSecretKey}")
+    private String webhookSecretKey;
 
     @PostMapping
     public ResponseEntity<?> checkout(
             @Valid @RequestBody CheckOutRequest request
             ) {
         return ResponseEntity.ok(checkoutService.checkout(request));
+    }
+
+    @PostMapping("/webhook")
+    public ResponseEntity<Void> handleWebhook(
+            @RequestHeader("Stripe-Signature") String signature,
+            @RequestBody String payload
+    ) {
+        try {
+            Event event = Webhook.constructEvent(payload, signature, webhookSecretKey);
+            StripeObject stripeObject = event.getDataObjectDeserializer().getObject().orElse(null);
+            switch (event.getType()) {
+                case "payment_intent.succeeded" -> {
+
+                }
+                case "payment_intent.failed" -> {
+
+                }
+            }
+            return ResponseEntity.ok().build();
+        } catch (SignatureVerificationException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @ExceptionHandler(PaymentException.class)
