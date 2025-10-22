6237ad88029f7dc1127ce7b48095005e7c981c77
yamchips <wflunch@gmail.com>
2025-10-22 21:28:57 +0100

    Update order status

--- src/main/java/com/codewithmosh/store/controllers/CheckOutController.java
+++ src/main/java/com/codewithmosh/store/controllers/CheckOutController.java
@@ -3,13 +3,17 @@ package com.codewithmosh.store.controllers;
 import com.codewithmosh.store.dtos.CheckOutRequest;
 import com.codewithmosh.store.dtos.CheckOutResponse;
 import com.codewithmosh.store.dtos.ErrorDto;
+import com.codewithmosh.store.entities.Order;
+import com.codewithmosh.store.entities.OrderStatus;
 import com.codewithmosh.store.exceptions.CartEmptyException;
 import com.codewithmosh.store.exceptions.CartNotFoundException;
 import com.codewithmosh.store.exceptions.PaymentException;
+import com.codewithmosh.store.repositories.OrderRepository;
 import com.codewithmosh.store.services.CheckoutService;
 import com.stripe.exception.SignatureVerificationException;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Event;
+import com.stripe.model.PaymentIntent;
 import com.stripe.model.StripeObject;
 import com.stripe.net.Webhook;
 import jakarta.validation.Valid;
@@ -26,6 +30,7 @@ import org.springframework.web.bind.annotation.*;
 public class CheckOutController {
 
     private final CheckoutService checkoutService;
+    private final OrderRepository orderRepository;
 
     @Value("${stripe.webhookSecretKey}")
     private String webhookSecretKey;
@@ -47,7 +52,13 @@ public class CheckOutController {
             StripeObject stripeObject = event.getDataObjectDeserializer().getObject().orElse(null);
             switch (event.getType()) {
                 case "payment_intent.succeeded" -> {
-
+                    var paymentIntent = (PaymentIntent) stripeObject;
+                    if (paymentIntent != null) {
+                        String orderId = paymentIntent.getMetadata().get("order_id");
+                        Order order = orderRepository.findById(Long.valueOf(orderId)).orElseThrow();
+                        order.setStatus(OrderStatus.PAID);
+                        orderRepository.save(order);
+                    }
                 }
                 case "payment_intent.failed" -> {
 
--- src/main/java/com/codewithmosh/store/services/StripePaymentGateway.java
+++ src/main/java/com/codewithmosh/store/services/StripePaymentGateway.java
@@ -23,7 +23,8 @@ public class StripePaymentGateway implements PaymentGateway{
             var builder = SessionCreateParams.builder()
                     .setMode(SessionCreateParams.Mode.PAYMENT)
                     .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
-                    .setCancelUrl(websiteUrl + "/checkout-cancel");
+                    .setCancelUrl(websiteUrl + "/checkout-cancel")
+                    .putMetadata("order_id", order.getId().toString());
 
             order.getItems().forEach(item -> {
                 var lineItem = createLineItem(item);
